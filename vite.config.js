fa16ff1bf77beb8ddf0a19db2c7b7b9e6ca6fbbe
Super7000 <subratachowdhurysuper@gmail.com>
2024-10-01 10:09:27 +0530

    modified vite config for github pages hosting

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    base: "/Canvas-Text-Editor/dist/",
+    server: {
+        open: true,
+    },
 })
